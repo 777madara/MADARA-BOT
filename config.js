901064fede19761242acc96a25343f08fe2c8b7e
TOMURA <m07062070@gmail.com>
2025-08-14 00:17:37 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "SHABAN-MD~bANBAZCI#D2hlRNdw7684lLkvLFVrYs4okj6KO4MEM50vKMNY5JI",
+SESSION_ID: process.env.SESSION_ID || "SHABAN-MD~T45BDTIJ#9niygoqxzkS5ngohWiVREMqKuSaxGCYZ5NgMWAAXg3Q",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
